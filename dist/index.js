2a32b9a0d2966d9e34c65f2df45757d044b6253d
icecrimu <charles.banigon@gmail.com>
2022-04-21 14:31:55 +0800

    Basic Types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = "Hello World";
+let isPublished = true;
+let x = "Hello";
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "hello"];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+// Basic Types
 let id: number = 5
+let company: string = "Hello World"
+let isPublished: boolean = true
+let x: any = "Hello"
+
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, "hello"]
