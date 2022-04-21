8260661f12255155949c218a7fdc10da4a53d77b
icecrimu <charles.banigon@gmail.com>
2022-04-21 14:38:34 +0800

    Tuple & Tuple Array

--- dist/index.js
+++ dist/index.js
@@ -6,3 +6,10 @@ let isPublished = true;
 let x = "Hello";
 let ids = [1, 2, 3, 4, 5];
 let arr = [1, true, "hello"];
+// Tuple
+let person = [1, "Charles", true];
+// Tuple Array
+let employee = [
+    [1, "Charles"],
+    [2, "Gemuel"]
+];
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,12 @@ let x: any = "Hello"
 
 let ids: number[] = [1, 2, 3, 4, 5]
 let arr: any[] = [1, true, "hello"]
+
+// Tuple
+let person: [number, string, boolean] = [1, "Charles", true]
+
+// Tuple Array
+let employee: [number, string][] = [
+  [1, "Charles"],
+  [2, "Gemuel"]
+]
